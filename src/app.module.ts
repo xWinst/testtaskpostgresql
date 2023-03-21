69f3c4a63c60507ed79bb268a44f6d078ec9ac96
Oleg <hellreebok@gmail.com>
2023-03-21 09:03:36 +0200

    add Sequelize, Config modules and .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
-import { Module } from '@nestjs/common';
-
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { SequelizeModule } from "@nestjs/sequelize";
 
 @Module({
-  imports: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: ".env",
+        }),
+        SequelizeModule.forRoot({
+            dialect: "postgres",
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            models: [],
+            autoLoadModels: true,
+        }),
+    ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+const { PORT = 4000 } = process.env;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(4000);
+    await app.listen(PORT, () => {
+        console.log(`Server running. Use our API on port: ${PORT}`);
+    });
 }
 bootstrap();
